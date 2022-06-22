e59a4cc37698e54aa508313b0dd218efb38cc2ec
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2022-06-23 00:13:17 +0800

    change name

--- src/components/layout/TheHearder.vue
+++ src/components/layout/TheHearder.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <nav>
-      <h1><router-link to="/"> Find a Coach</router-link></h1>
+      <h1><router-link to="/"> Coachlist </router-link></h1>
       <ul>
         <li><router-link to="/coaches">All Coaches</router-link></li>
         <li v-if="!isLoggedIn">
